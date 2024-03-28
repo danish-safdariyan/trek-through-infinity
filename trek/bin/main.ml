612637bcf4737b530494ddaf97b074959ff578aa
Gabriella Best <gb486@cornell.edu>
2024-03-27 21:44:59 -0400

    Implemented user interface (currently faulty)

--- trek/bin/main.ml
+++ trek/bin/main.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World!"
+open Trek
+
+let () =
+  print_endline "Hi! Please submit a name for your event:";
+  let user_event = read_line () in
+  print_endline
+    "Please submit a date in the form [month][day][year]. \n\
+     For example, to submit the date March 27, 2024, you would type [03272024]";
+  let user_day = read_line () in
+  print_endline
+    ("You have successfully added the event " ^ user_event ^ " on " ^ user_day
+   ^ " to your calendar.");
+   let () = CalDict.insert user_day user_event;
