532add6d9978ee2464a18a7e6fe358c69f8877e3
Gabriel <gm546@cornell.edu>
2024-03-27 22:00:52 -0400

    Fixing part of main, still needs implementation

--- trek/bin/main.ml
+++ trek/bin/main.ml
@@ -1,13 +1,18 @@
 open Trek
 
+let () = print_endline "Hi! Please submit a name for your event!"
+let user_event = read_line ()
+
 let () =
-  print_endline "Hi! Please submit a name for your event:";
-  let user_event = read_line () in
   print_endline
-    "Please submit a date in the form [month][day][year]. \n\
-     For example, to submit the date March 27, 2024, you would type [03272024]";
-  let user_day = read_line () in
+    "Please submit a date in the form\n\
+    \   [month][day][year]. \n\
+     For example, to submit the date March 27, 2024, you\n\
+    \   would type [03272024]"
+
+let user_day = read_line ()
+
+let () =
   print_endline
     ("You have successfully added the event " ^ user_event ^ " on " ^ user_day
-   ^ " to your calendar.");
-   let () = CalDict.insert user_day user_event;
+   ^ " to your calendar.")
