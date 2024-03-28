876c8250d3826172e7a8da62ab4c4e52cb852a44
Gabriel <gm546@cornell.edu>
2024-03-28 17:09:46 -0400

    Starting process of taking user input for GUI output in main.

--- trek/bin/main.ml
+++ trek/bin/main.ml
@@ -1,18 +1,16 @@
 open Trek
 
-(* let () = print_endline "Hi! Please submit a name for your event!" let
-   user_event = read_line ()
+let () = print_endline "Hi! Please submit a name for your event!"
+let user_event = read_line ()
 
-   let () = print_endline "Please submit a date in the form\n\ \
-   [month][day][year]. \n\ For example, to submit the date March 27, 2024,
-   you\n\ \ would type [03272024]"
+let () =
+  print_endline
+    ("Now please select a day from Day 01 to Day 35 for your event to be on.\n"
+   ^ "Please follow the format below: \n"
+   ^ "\"Day 0X\" for single digit days, and then for double digit, \"Day XX\"")
 
-   let user_day = read_line ()
-
-   let () = print_endline ("You have successfully added the event " ^ user_event
-   ^ " on " ^ user_day ^ " to your calendar.") *)
-
-(* let _ = Example.example13 () *)
+let user_day = read_line ()
+let () = print_endline (user_event ^ " " ^ user_day)
 
 let _ =
   let default_event = "Nothing" (* Define your default event here *) in
