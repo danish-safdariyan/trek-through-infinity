5f129e6cf72f256cd1c7f62f5e68ee773396553b
Kaz Tam <kt557@cornell.edu>
2024-05-10 18:30:50 -0400

    Opened Backend in appropriate files and cleaned up code

--- trek/lib/frontend/buttonDisplay.ml
+++ trek/lib/frontend/buttonDisplay.ml
@@ -1,4 +1,5 @@
 open Bogue
+open Backend
 module W = Widget
 module L = Layout
 
@@ -21,7 +22,7 @@ let prev_btn get_month update_month =
     ~label:(Label.create ~align:Draw.Center "<")
     ~action:(fun _ ->
       let m, y = get_month () in
-      update_month (Backend.Date.prev_month m y))
+      update_month (Date.prev_month m y))
     "Prev month"
 
 let next_btn get_month update_month =
@@ -29,5 +30,5 @@ let next_btn get_month update_month =
     ~label:(Label.create ~align:Draw.Center ">")
     ~action:(fun _ ->
       let m, y = get_month () in
-      update_month (Backend.Date.next_month m y))
+      update_month (Date.next_month m y))
     "Next month"
