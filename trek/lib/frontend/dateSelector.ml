3954601e59c30eadede0ddd856eb0813797dd1d4
Kaz Tam <kt557@cornell.edu>
2024-05-12 19:36:09 -0400

    Got dateSelector to work!

--- trek/lib/frontend/dateSelector.ml
+++ trek/lib/frontend/dateSelector.ml
@@ -7,22 +7,117 @@ module P = Popups
 
 type sel_info = {
   mutable date : Date.t;
-  mutable cur_btn : Button.t;
+  mutable cur_btn : Box.t;
   mutable on_update : Date.t -> unit;
 }
 
+module type BtnSig = sig
+  type t
+
+  val make_btn : Date.t -> Date.month -> sel_info -> t
+  (** Makes a button for the date *)
+
+  val get_layout : t -> L.t
+  (** Returns the layout associated with the button *)
+
+  val change_btn : t -> Date.t -> Date.month -> sel_info -> unit
+  (** Changes the button into the button with the provided info; layout will
+      update. *)
+
+  val empty_box : Box.t
+  (** An empty box. *)
+end
+
+module Btn : BtnSig = struct
+  type t = {
+    layout : L.t;
+    label : W.t;
+    box : Box.t;
+    mutable d : Date.t;
+  }
+
+  let w, h, size = (20, 20, 15)
+
+  (** Style of box when this is not the selected button. *)
+  let off_style =
+    Style.create
+      ~background:(Draw.opaque Draw.white |> Style.color_bg)
+      ~border:
+        (Style.mk_line ~color:(Draw.opaque Draw.dark_grey) ~width:2 ()
+        |> Style.mk_border)
+      ()
+
+  (** Style of box when this is the selected button. *)
+  let on_style =
+    Style.create
+      ~background:(Draw.opaque Draw.pale_grey |> Style.color_bg)
+      ~border:
+        (Style.mk_line ~color:(Draw.opaque Draw.dark_grey) ~width:2 ()
+        |> Style.mk_border)
+      ()
+
+  let empty_box = Box.create ()
+  let toggle_on box = Box.set_style box on_style
+  let toggle_off box = Box.set_style box off_style
+  let get_layout btn = btn.layout
+
+  let make_btn (d : Date.t) m info =
+    let label =
+      W.label ~size
+        ~fg:
+          (if d.month = m then Draw.opaque Draw.black else Draw.opaque Draw.grey)
+        ~align:Draw.Center (string_of_int d.day)
+    in
+    let box = W.box ~w ~h ~style:off_style () in
+    let l_layout = L.resident label in
+    let out =
+      {
+        layout = L.superpose [ L.resident box; l_layout ];
+        label;
+        box = W.get_box box;
+        d;
+      }
+    in
+    let _ =
+      if d = info.date then (
+        W.get_box box |> toggle_on;
+        info.cur_btn <- W.get_box box)
+      else ();
+      W.on_click
+        ~click:(fun _ ->
+          let b = W.get_box box in
+          if Box.get_style b = off_style then (
+            toggle_off info.cur_btn;
+            toggle_on b;
+            info.cur_btn <- b;
+            info.date <- out.d)
+          else ();
+          info.on_update d)
+        label;
+      Space.full_width l_layout
+    in
+    out
+
+  let change_btn btn (d : Date.t) m info =
+    Sync.push (fun () ->
+        W.set_text btn.label (string_of_int d.day);
+        Label.set_fg_color (W.get_label btn.label)
+          (if d.month = m then Draw.opaque Draw.black else Draw.opaque Draw.grey);
+        if d = info.date then (
+          info.cur_btn <- btn.box;
+          toggle_on btn.box)
+        else toggle_off btn.box;
+        btn.d <- d)
+end
+
 type t = {
   layout : L.t;
-  m_layout : L.t;
+  m_lst : Btn.t list;
   m_label : W.t;
   info : sel_info;
   mutable cur_m : Date.month;
   mutable cur_y : int;
 }
-
-let get_btn info = info.cur_btn
-let get_m sel = sel.cur_m
-let get_y sel = sel.cur_y
 
 (** Gets a 6-week month. *)
 let get_month m y =
@@ -46,47 +141,22 @@ let get_month m y =
   in
   last_week 0 [ last ] |> List.rev |> add_month |> fst_week
 
-(** Returns a button layout that represents a day. *)
-let layout_of_day (d : Date.t) m info =
-  let s = string_of_int d.day in
-  let s = if String.length s < 2 then s ^ " " else s in
-  let num = W.label ~align:Draw.Center s in
-  let _ =
-    if d.month = m then ()
-    else Label.set_fg_color (W.get_label num) (Draw.opaque Draw.grey)
+let get_month_lst info m y =
+  let days = get_month m y in
+  let rec helper = function
+    | [] -> []
+    | h :: t -> Btn.make_btn h m info :: helper t
   in
-  let btn =
-    W.button ~kind:Button.Switch ~label:(W.get_label num)
-      ~bg_on:(Style.Solid (Draw.opaque Draw.pale_grey))
-      ~bg_off:(Style.Solid (Draw.opaque Draw.white))
-      ~state:(d = info.date)
-      ~border_color:(Draw.opaque Draw.dark_grey)
-      "Button"
-  in
-  let _ =
-    if d = info.date then info.cur_btn <- W.get_button btn;
-    W.on_button_release
-      ~release:(fun w ->
-        let btn = W.get_button w in
-        if Button.state btn then (
-          info.date <- d;
-          get_btn info |> Button.reset;
-          info.cur_btn <- btn)
-        else ();
-        info.on_update d)
-      btn
-  in
-  L.resident btn
+  helper days
 
 (** Returns a layout of a month with the specified info. *)
-let layout_of_month info m y =
-  let days = get_month m y in
+let layout_of_month month_lst =
   let week_layout days =
-    let rec helper info = function
+    let rec helper = function
       | [] -> []
-      | h :: t -> layout_of_day h m info :: helper info t
+      | h :: t -> Btn.get_layout h :: helper t
     in
-    helper info days |> L.flat ~margins:0
+    helper days |> L.flat ~margins:0
   in
   let rec helper = function
     | [] -> []
@@ -94,24 +164,32 @@ let layout_of_month info m y =
         week_layout [ d1; d2; d3; d4; d5; d6; d7 ] :: helper t
     | lst -> [ week_layout lst ]
   in
-  helper days |> L.tower ~margins:0
+  helper month_lst |> L.tower ~margins:0
 
 (** Sets the display of [sel] to the month and year specified. *)
 let update_display sel =
-  L.set_rooms sel.m_layout [ layout_of_month sel.info sel.cur_m sel.cur_y ];
-  Sync.push (fun () ->
-      W.set_text sel.m_label
-        (Date.string_of_month sel.cur_m ^ " " ^ string_of_int sel.cur_y))
+  let days = get_month sel.cur_m sel.cur_y in
+  let rec helper = function
+    | [], [] -> ()
+    | btn :: t1, d :: t2 ->
+        let _ = Btn.change_btn btn d sel.cur_m sel.info in
+        helper (t1, t2)
+    | _ -> failwith "Should be same length"
+  in
+  helper (sel.m_lst, days);
+  W.set_text sel.m_label
+    (Date.string_of_month sel.cur_m ^ " " ^ string_of_int sel.cur_y)
 
 let make_selector () =
   let info =
     {
       date = Date.current_date ();
-      cur_btn = Button.create Button.Switch "";
+      cur_btn = Btn.empty_box;
       on_update = (fun _ -> ());
     }
   in
-  let m_layout = layout_of_month info info.date.month info.date.year in
+  let m_lst = get_month_lst info info.date.month info.date.year in
+  let m_layout = layout_of_month m_lst in
   let m_label =
     W.label ~align:Draw.Center
       (Date.string_of_month info.date.month ^ " " ^ string_of_int info.date.year)
@@ -124,7 +202,7 @@ let make_selector () =
   let next_btn =
     W.button ~kind:Button.Trigger
       ~label:(Label.create ~align:Draw.Center ">")
-      "Next btn"
+      "Next\n     btn"
   in
   let top = L.flat_of_w [ prev_btn; m_label; next_btn ] in
   let stuff = L.tower [ top; m_layout ] in
@@ -132,7 +210,7 @@ let make_selector () =
   let sel =
     {
       layout = L.superpose [ box; stuff ];
-      m_layout;
+      m_lst;
       m_label;
       info;
       cur_m = info.date.month;
@@ -143,14 +221,14 @@ let make_selector () =
     Space.full_width top;
     W.on_button_release
       ~release:(fun _ ->
-        let m, y = Date.prev_month (get_m sel) (get_y sel) in
+        let m, y = Date.prev_month sel.cur_m sel.cur_y in
         sel.cur_m <- m;
         sel.cur_y <- y;
         update_display sel)
       prev_btn;
     W.on_button_release
       ~release:(fun _ ->
-        let m, y = Date.next_month (get_m sel) (get_y sel) in
+        let m, y = Date.next_month sel.cur_m sel.cur_y in
         sel.cur_m <- m;
         sel.cur_y <- y;
         update_display sel)
