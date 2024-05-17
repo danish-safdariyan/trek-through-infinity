54a55aa0e436a2b3db27431084b07cf4ade8dc0e
Kaz Tam <kt557@cornell.edu>
2024-05-16 22:52:21 -0400

    Added genDisplay.mli

---
+++ trek/lib/frontend/genDisplay.mli
@@ -0,0 +1,17 @@
+open Bogue
+
+val surrounding_box :
+  ?background:Draw.color ->
+  ?border_color:Draw.color ->
+  ?width:int ->
+  Layout.t ->
+  Layout.t
+(** Creates a box that surounds the layout; Default background is a shade of
+    turquoise "#00B9BC", default border is dark_turquoise "#003738". *)
+
+val theme_box : ?width:int -> int -> int -> Layout.t
+(** Creates a box with the theme colors using the specified width and height. *)
+
+val adjust_label_size : int -> Widget.t -> unit
+(** [adjust_label_size max_size label] cuts off [label] if it is over
+    [max_size]. *)
