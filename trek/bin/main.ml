b807de4720849b15fc45d22227ee83361681e172
Gabriel <gm546@cornell.edu>
2024-05-16 22:50:42 -0400

    Added author statement.

--- trek/bin/main.ml
+++ trek/bin/main.ml
@@ -1,3 +1,6 @@
+(* @author Gabriel Martinez-Amezaga (gm546), @author Kaz Tam (kt557), @author
+   Danish Safdariyan (ds968), @author Andisha Safdariyan (as3254), @author
+   Gabriella Best (gb486) *)
 open Frontend
 
 let _ = Display.calendar_display ()
