2cad108dbe895a13bcf6d34b9c95f56b3ba9677f
Gabriel <gm546@cornell.edu>
2024-05-16 20:31:09 -0400

    Added date selection for tasks.

--- trek/lib/frontend/taskListDisplay.ml
+++ trek/lib/frontend/taskListDisplay.ml
@@ -18,7 +18,9 @@ let layout_of_task w task =
 
 (** Popup for adding tasks *)
 let addTaskPopup layout update_task_list =
-  let title_input = W.text_input ~prompt:"Task Title" () in
+  let title_input = W.text_input ~prompt:"Task Title (Input Here)" () in
+  let selector = DateSelector.make_selector () in
+  let date_input = W.button (Date.current_date () |> Date.format_date) in
   let add_btn = W.button "Add Task" in
   let cancel_btn = W.button "Cancel" in
   let buttons = L.flat_of_w [ add_btn; cancel_btn ] in
@@ -27,12 +29,17 @@ let addTaskPopup layout update_task_list =
       [
         W.label ~size:18 "New Task:" |> L.resident;
         title_input |> L.resident;
+        date_input |> L.resident;
         buttons;
       ]
   in
   let popup = L.superpose [ GenDisplay.surrounding_box content; content ] in
   let out = P.attach_popup layout popup in
+  let date_selector_popup =
+    P.attach_popup layout (DateSelector.get_layout selector)
+  in
   let on_close () =
+    P.hide date_selector_popup;
     P.hide out;
     W.set_text title_input ""
   in
@@ -41,10 +48,27 @@ let addTaskPopup layout update_task_list =
     W.on_button_release
       ~release:(fun _ ->
         let task_title = W.get_text_input title_input |> Text_input.text in
-        if task_title <> "" then update_task_list task_title;
+        let date = W.get_text_input date_input |> Text_input.text in
+        (try
+           if task_title <> "" && date <> "" then
+             update_task_list task_title date
+         with _ -> ());
+        (*CIRCLE BACK TO DATE ERROR HANDLING LATER*)
         on_close ())
       add_btn;
-    P.should_exit_on_press out true
+    P.should_exit_on_press out true;
+    P.should_exit_on_press date_selector_popup true;
+    DateSelector.on_update selector (fun _ ->
+        W.set_text date_input
+          (DateSelector.get_date selector |> Date.format_date);
+        P.hide date_selector_popup);
+    W.on_button_release
+      ~release:(fun _ ->
+        if P.get_state date_selector_popup then (
+          P.hide date_selector_popup;
+          DateSelector.get_date selector |> DateSelector.set_date selector)
+        else P.show date_selector_popup)
+      date_input
   in
   out
 
@@ -56,8 +80,9 @@ let task_list_layout w h tList update_task_list =
       (fun task -> layout_of_task (w - 20) task)
       (TaskList.get_tasks today tList)
   in
-  let add_tsk_btn = W.button "+" in
+  let add_tsk_btn = W.button "Add" in
   let header = L.flat [ label; add_tsk_btn |> L.resident ] in
+  let () = Space.full_width header in
   let room_list = header :: task_list in
   let taskLayout =
     L.superpose [ GenDisplay.theme_box w h; L.tower room_list ]
--- trek/lib/frontend/taskListDisplay.mli
+++ trek/lib/frontend/taskListDisplay.mli
@@ -2,4 +2,4 @@ open Bogue
 open Backend
 
 val left_side_layout :
-  int -> int -> Layout.t -> TaskList.t -> (string -> unit) -> Layout.t
+  int -> int -> Layout.t -> TaskList.t -> (string -> string -> unit) -> Layout.t
